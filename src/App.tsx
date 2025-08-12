4a161756d9a35e7ce74fbd48f599d65dadacd807
HasikaSeethu <hasikaseethu@gmail.com>
2025-08-12 09:33:30 +0530

    Fix missing index.css and App.tsx diff markers

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,26 @@
-@@ .. @@
- import React from 'react';
- import Navigation from './components/Navigation';
- import Hero from './components/Hero';
- import About from './components/About';
- import Services from './components/Services';
- import Projects from './components/Projects';
- import Contact from './components/Contact';
- import Footer from './components/Footer';
+import React from 'react';
+import Navigation from './components/Navigation';
+import Hero from './components/Hero';
+import About from './components/About';
+import Services from './components/Services';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
- function App() {
-+  console.log('App component rendering');
-+  
-   return (
-     <div className="font-inter">
-       <Navigation />
-       <section id="hero">
-         <Hero />
-       </section>
-       <About />
-       <Services />
-       <Projects />
-       <Contact />
-       <Footer />
-     </div>
-   );
- }
+function App() {
+  return (
+    <div className="font-inter">
+      <Navigation />
+      <section id="hero">
+        <Hero />
+      </section>
+      <About />
+      <Services />
+      <Projects />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
 
- export default App;
+export default App;
